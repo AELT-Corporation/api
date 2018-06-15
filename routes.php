df2df52ab62f84914e66ac3e5d7ab192bdd4cf1d
LuisMiara <luisgustavomiara@gmail.com>
2018-06-15 19:59:51 -0300

    (API, Web) => Estruturação do projeto

--- routes.php
+++ routes.php
@@ -9,21 +9,21 @@ $app->group('/v1', function() {
      * Dentro de v1, o recurso /user
      */
     $this->group('/user', function() {
-        $this->get('', '\App\v1\Controllers\UserController:listUser');
-        $this->post('', '\App\v1\Controllers\UserController:createUser');
+        $this->get('', '\App\Controllers\UserController:listUser');
+        $this->post('', '\App\Controllers\UserController:createUser');
         
         /**
          * Validando se tem um integer no final da URL
          */
-        $this->get('/{id:[0-9]+}', '\App\v1\Controllers\UserController:viewUser');
-        $this->put('/{id:[0-9]+}', '\App\v1\Controllers\UserController:updateUser');
-        $this->delete('/{id:[0-9]+}', '\App\v1\Controllers\UserController:deleteUser');
+        $this->get('/{id:[0-9]+}', '\App\Controllers\UserController:viewUser');
+        $this->put('/{id:[0-9]+}', '\App\Controllers\UserController:updateUser');
+        $this->delete('/{id:[0-9]+}', '\App\Controllers\UserController:deleteUser');
     });
 
     /**
      * Dentro de v1, o recurso /auth
      */
     $this->group('/auth', function() {
-        $this->get('', \App\v1\Controllers\AuthController::class);
+        $this->get('', \App\Controllers\AuthController::class);
     });
 });
